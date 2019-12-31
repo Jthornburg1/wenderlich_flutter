d631e5ebeba47255b12ac60ee02c48fd2231fce1
Jon Thornburg <reliable2192@gmail.com>
2019-12-30 19:54:25 -0500

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'ghflutter.dart';
+import 'package:flutter/material.dart';
+import 'strings.dart';
+
+void main() => runApp(GHFlutterApp());
+
+
+class GHFlutterApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: Strings.appTitle,
+      theme: ThemeData(primaryColor: Colors.green.shade800),
+      home: GHFlutter(),
+    );
+  }
+}
+
